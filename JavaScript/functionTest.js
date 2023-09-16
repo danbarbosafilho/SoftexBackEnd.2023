37c920875fceaa6e0227c9346907fbe03929cff0
Daniel Barbosa <danielbarbosaflh@gmail.com>
2023-09-16 12:07:56 -0300

    Create functionTest.js

---
+++ JavaScript/functionTest.js
@@ -0,0 +1,13 @@
+//Função simples sem parâmetro
+function simpleFunction() {
+    console.log('Hello, World!');
+}
+
+//Função com parâmetro e retorno
+function somaFunction(n1, n2) {
+    return n1 + n2;
+}
+
+//Arrow Function com parâmetros e retorno
+const ArrowSoma = (n1, n2) => n1 + n2;
+
